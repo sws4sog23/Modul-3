26e728aef5035fa341de6961af9c9cf2a2ac3f68
sws4sog23 <sws4sog23@gmail.com>
2024-08-15 23:17:55 +0300

    Add files via upload

---
+++ Modul_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(st):
+    count_calls()
+    st_ = (len(st), st.upper(), st.lower())
+    return st_
+
+def is_contains(st, sp):
+    count_calls()
+    st = st.lower()
+    sp_ = [x.lower() for x in sp]
+
+    if st in sp_:
+        fl = True
+    else:
+        fl = False
+    return fl
+
+print(string_info('caT'))
+print(string_info('VolKi'))
+print(is_contains('aa', ['aA', 'Ga', 'wHw']))
+print(is_contains('aB', ['aA', 'Ga', 'wHw']))
+print(calls)
