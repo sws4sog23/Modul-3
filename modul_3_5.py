0364fbea0268c90e9928c21d76cc9c4c94abb854
sws4sog23 <sws4sog23@gmail.com>
2024-08-21 00:48:35 +0300

    Add files via upload

--- modul_3_5.py
+++ modul_3_5.py
@@ -1,5 +1,6 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str_number.replace('0', '')
     first = int(str_number[0])
     if len(str_number) == 1:
         return first
@@ -8,6 +9,6 @@ def get_multiplied_digits(number):
             return first * get_multiplied_digits(int(str_number[1:]))
 
 
-result = get_multiplied_digits(1023006)
+result = get_multiplied_digits(4020336590)
 print(result)
 
