11195be6fb193d5488b4988be9f7bee1edffe6c5
sws4sog23 <sws4sog23@gmail.com>
2024-09-01 16:28:52 +0300

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,41 +1,34 @@
 data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-sum_ = 0
-spis = []
-v = spis
-
-
+sum_=0
+spis=[]
+v=spis
 def sum_t(sp):
     global sum_
     global spis
     for s in sp:
         if type(s) is int:
-            sum_ += s
+            sum_+=s
         if type(s) is str:
-            sum_ += len(s)
+            sum_+=len(s)
     return sum_
-
-
 def calculate_structure_sum(*args):
     cal(args)
     return sum_t(v)
-
-
 def cal(*args):
-    global sum_
     for i in args:
-        if isinstance(i, (int, str)):
+        if isinstance(i, (int,str)):
             spis.append(i)
             return cal(args[1:])
         if isinstance(i, dict):
             i = list(i.items())
         for j in i:
-            cal(j)
+           cal(j)
 
 
 result = calculate_structure_sum(data_structure)
